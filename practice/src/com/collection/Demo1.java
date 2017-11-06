7040810da7636ca3b7e08a17ed49bc2926aaacb9
zhuwei302 <244840434@qq.com>
2017-11-06 18:50:51 +0800

    init

--- practice/src/com/collection/Demo1.java
+++ practice/src/com/collection/Demo1.java
@@ -1,13 +1,19 @@
 package com.collection;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by zhuwei on 2017/10/14 0014.
  */
 public class Demo1 {
     List list = new ArrayList<>();
-    Map map = new HashMap();
     Set set = new HashSet();
     Set tset = new TreeSet();
+
+    Map map = new HashMap(); //非线程安全，可以接受null的键、值
+    Map map1 = new Hashtable(); //部分方法 synchronized 修饰，线程安全、键、值不能为空
+    Map map2 = new ConcurrentHashMap();
+    StringBuffer sb = new StringBuffer();  //方法有synchronized修饰，线程安全
+    StringBuilder sb1 = new StringBuilder(); //非线程安全，效率高
 }
